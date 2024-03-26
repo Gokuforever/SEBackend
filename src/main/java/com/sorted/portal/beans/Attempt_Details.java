16715b962bcda1217b090e018cced38526fb5aba
AVANTGARDE\yogeshkh <yogeshkh@safexpay.com>
2024-03-27 01:30:22 +0530

    Attemp_Details:: new bean added

---
+++ src/main/java/com/sorted/portal/beans/Attempt_Details.java
@@ -0,0 +1,21 @@
+package com.sorted.portal.beans;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Attempt_Details {
+
+	private String otp;
+	private LocalDateTime attempt_date;
+	private String ip_address;
+}
