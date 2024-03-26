28bc3d08068445b719f50b7c12d51d776633fe6a
AVANTGARDE\yogeshkh <yogeshkh@safexpay.com>
2024-03-27 01:28:23 +0530

    SERegExpUtils:: new regex validations added

--- src/main/java/com/sorted/portal/utils/SERegExpUtils.java
+++ src/main/java/com/sorted/portal/utils/SERegExpUtils.java
@@ -6,19 +6,25 @@ import lombok.NonNull;
 
 public class SERegExpUtils {
 
-	public static Boolean isString(@NonNull String val) {
+	public static boolean isString(@NonNull String val) {
 		val = val.trim();
 		Pattern pattern = Pattern.compile("[a-zA-Z]*");
 		return pattern.matcher(val).matches();
 	}
-	
-	public static Boolean isMobileNo(@NonNull String val) {
+
+	public static boolean isMobileNo(@NonNull String val) {
 		val = val.trim();
 		Pattern pattern = Pattern.compile("[6-9]\\d{9}");
 		return pattern.matcher(val).matches();
 	}
+	
+	public static boolean isOtp(@NonNull String val) {
+		val = val.trim();
+		Pattern pattern = Pattern.compile("\\d{6}");
+		return pattern.matcher(val).matches();
+	}
 
-	public static Boolean standardTextValidation(@NonNull String val) {
+	public static boolean standardTextValidation(@NonNull String val) {
 		val = val.strip();
 		if (val.length() > 255) {
 			return false;
@@ -27,7 +33,7 @@ public class SERegExpUtils {
 		return pattern.matcher(val).matches();
 	}
 
-	public static Boolean isQuantity(@NonNull String val) {
+	public static boolean isQuantity(@NonNull String val) {
 		val = val.strip();
 		if (val.length() > 10) {
 			return false;
@@ -50,9 +56,17 @@ public class SERegExpUtils {
 		if (value.length() > 13) {
 			return false;
 		}
-		// Pattern pattern = Pattern.compile("^\\d{1,10}.\\d{0,2}$");
 		Pattern pattern = Pattern.compile("^(?!0*\\.0*$)\\d{1,10}(?:\\.\\d{0,2})?$");
 		return pattern.matcher(value).matches();
+	}
+
+	public static boolean isEmail(String email_id) {
+		if (email_id == null || email_id.length() > 100) {
+			return false;
+		}
+		Pattern emailRegExp = Pattern.compile("^[a-zA-Z0-9\\.\\_\\-]{1,64}@[a-zA-Z0-9\\.\\-]{1,253}\\.[a-zA-Z]{2,4}$");
+
+		return emailRegExp.matcher(email_id).matches();
 	}
 
 	public static void main(String[] args) {
