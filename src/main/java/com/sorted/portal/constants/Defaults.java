1e7241d2829df52bfb7d5aa43d65707600cbd4d2
AVANTGARDE\yogeshkh <yogeshkh@safexpay.com>
2024-03-31 15:21:39 +0530

    new common methods and constants added

--- src/main/java/com/sorted/portal/constants/Defaults.java
+++ src/main/java/com/sorted/portal/constants/Defaults.java
@@ -5,4 +5,6 @@ public class Defaults {
 	public static final String DEF_USER_ERR_MSG = "Something went wrong";
 	public static final String SYSTEM_ADMIN = "System Admin";
 	public static final String SIGN_UP = "Sign Up";
+	public static final String SIGN_IN = "Sign In";
+	public static final String SMS_SERVICE = "Sms Service";
 }
--- src/main/java/com/sorted/portal/utils/CommonUtils.java
+++ src/main/java/com/sorted/portal/utils/CommonUtils.java
@@ -20,4 +20,19 @@ public class CommonUtils {
 	public static <T> List<T> convertS2L(Set<T> set) {
 		return set.stream().collect(Collectors.toList());
 	}
+
+	public static int generateFixedLengthRandomNumber(int length) {
+		if (length > 18) {
+			throw new IllegalStateException("To many digits");
+		}
+		int powInt = length - 1;
+		int tLen = (int) Math.pow(10, powInt) * 9;
+		int number = (int) (Math.random() * tLen) + (int) Math.pow(10, powInt) * 1;
+		String randomNumber = number + "";
+		if (randomNumber.length() != length) {
+			throw new IllegalStateException("The random number '" + randomNumber + "' is not '" + length + "' digits");
+		}
+		return number;
+	}
+
 }
