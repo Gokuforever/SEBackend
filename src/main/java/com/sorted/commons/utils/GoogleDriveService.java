34bf5e8f2347f0470cc6383b3f4637330a2e6339
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2024-09-07 22:47:34 +0530

    File transfer setup

--- src/main/java/com/sorted/commons/utils/GoogleDriveService.java
+++ src/main/java/com/sorted/commons/utils/GoogleDriveService.java
@@ -1,99 +1,69 @@
 package com.sorted.commons.utils;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.security.GeneralSecurityException;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.stereotype.Service;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.FileContent;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 
-@Component
+@Service
 public class GoogleDriveService {
-	private static final String APPLICATION_NAME = "AudioBook";
+
+	private static final String SERVICE_ACCOUNT_FILE = "D:\\google_auth\\service_account.json";
+	private static final String PARENT_FOLDER_ID = "1gzz7LRmeLT0bsJhF7OeuWfMkyBTgautX";
 	private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-	private static final String TOKENS_DIRECTORY_PATH = "tokens";
-	private static final List<String> SCOPES = Arrays.asList("https://www.googleapis.com/auth/drive");
-	private static final String CREDENTIALS_FILE_PATH = "E:/credentials/service_account.json";
 
-	/**
-	 * Creates an authorized Credential object.
-	 *
-	 * @param HTTP_TRANSPORT The network HTTP Transport.
-	 * @return An authorized Credential object.
-	 * @throws IOException If the credentials.json file cannot be found.
-	 */
-	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-		// Load client secrets from file
-		java.io.File credentialsFile = new java.io.File(CREDENTIALS_FILE_PATH);
-		if (!credentialsFile.exists()) {
-			throw new FileNotFoundException("File not found: " + CREDENTIALS_FILE_PATH);
-		}
-
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-				new InputStreamReader(new FileInputStream(credentialsFile)));
-
-		// Build flow and trigger user authorization request
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES)
-				.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-				.setAccessType("offline").build();
-
-		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+	private Drive getDriveService() throws GeneralSecurityException, IOException {
+		GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(SERVICE_ACCOUNT_FILE)) // Changed
+																												// to
+																												// FileInputStream
+				.createScoped(Collections.singleton(DriveScopes.DRIVE));
+		return new Drive.Builder(GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY,
+				new HttpCredentialsAdapter(credentials)).setApplicationName("My Google Drive App").build();
 	}
 
-	public Drive getInstance() throws GeneralSecurityException, IOException {
-		// Build a new authorized API client service.
-		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-		Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-				.setApplicationName(APPLICATION_NAME).build();
-		return service;
-	}
-
-	// Method to upload a photo
-	public String uploadPhoto(MultipartFile multipartFile) throws IOException, GeneralSecurityException {
-		Drive service = getInstance();
-		java.io.File tempFile = java.io.File.createTempFile("temp", ".tmp");
-		multipartFile.transferTo(tempFile);
+	public String uploadPhoto(String filePath) throws IOException, GeneralSecurityException {
+		Drive service = getDriveService();
 		File fileMetadata = new File();
 		fileMetadata.setName("Image");
-		fileMetadata.setParents(Collections.singletonList("1gzz7LRmeLT0bsJhF7OeuWfMkyBTgautX"));
+		fileMetadata.setParents(Collections.singletonList(PARENT_FOLDER_ID));
 
-		FileContent mediaContent = new FileContent("image/jpeg", tempFile);
+		java.io.File filePathObj = new java.io.File(filePath);
+		FileContent mediaContent = new FileContent("image/jpeg", filePathObj);
 
 		File file = service.files().create(fileMetadata, mediaContent).setFields("id").execute();
+		System.out.printf("File '%s' uploaded successfully with ID: %s%n", filePath, file.getId());
 		return file.getId();
 	}
 
-	// Method to fetch a photo
-	public void fetchPhoto(String fileId, String destinationPath) throws IOException, GeneralSecurityException {
-		Drive service = getInstance();
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		service.files().get(fileId).executeMediaAndDownloadTo(outputStream);
-
-		try (FileOutputStream fos = new FileOutputStream(destinationPath)) {
-			outputStream.writeTo(fos);
+	public void fetchPhoto(String fileId, String destinationPath) {
+		try {
+			Drive service = getDriveService();
+			Drive.Files.Get request = service.files().get(fileId);
+			try (InputStream inputStream = request.executeMediaAsInputStream();
+					FileOutputStream outputStream = new FileOutputStream(destinationPath)) {
+				byte[] buffer = new byte[1024];
+				int bytesRead;
+				while ((bytesRead = inputStream.read(buffer)) != -1) {
+					outputStream.write(buffer, 0, bytesRead);
+				}
+				System.out.printf("File downloaded successfully to '%s'%n", destinationPath);
+			}
+		} catch (Exception e) {
+			System.out.println("An error occurred: " + e.getMessage());
 		}
 	}
 }
