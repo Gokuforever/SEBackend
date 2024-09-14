a89698bd5b04c0cf492096a9ed76ff4c1e809d34
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2024-09-15 01:58:02 +0530

    CorsConfig:: new url added

--- src/main/java/com/sorted/commons/config/CorsConfig.java
+++ src/main/java/com/sorted/commons/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowedOrigins(Arrays.asList("https://stz-frontend-service-ts.vercel.app", // Vercel frontend
 				"http://localhost:5173", // Local development
-				"https://stz-frontend-service-ts-git-749429-anand-suryawanshis-projects.vercel.app",
+				"https://vinayak.studeaze.in",
 				"https://seller.studeaze.in"
 		));
 
