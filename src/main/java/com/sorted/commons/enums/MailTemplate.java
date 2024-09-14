a341c35c4dfeb13bf5dfff59571f4dcb7c3b1f93
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2024-09-14 05:34:56 +0530

    MailTemplate:: new template added

--- src/main/java/com/sorted/commons/enums/MailTemplate.java
+++ src/main/java/com/sorted/commons/enums/MailTemplate.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum MailTemplate {
-	SIGN_UP_COMPLETED("sign_up_completed.html", "Welcom to the world for StudEaze.");
+	SIGN_UP_COMPLETED("sign_up_completed.html", "Welcom to the world for StudEaze."),
+	SELLER_WELCOME_MAIL("welcome_mail.html", "Welcom to StudEaze.");
 
 	@Getter
 	private String file_name;
