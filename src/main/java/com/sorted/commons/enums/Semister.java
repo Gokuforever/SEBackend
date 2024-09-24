fd1a84a2b1d5b2cf9a59b1c08426a984ebfa6c81
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2024-09-24 23:00:21 +0530

    Semister:: new enum added

---
+++ src/main/java/com/sorted/commons/enums/Semister.java
@@ -0,0 +1,34 @@
+package com.sorted.commons.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+
+@Getter
+@AllArgsConstructor
+public enum Semister {
+
+	SEM_1("Sem 1"), SEM_2("Sem 2"), SEM_3("Sem 3"), SEM_4("Sem 4"), SEM_5("Sem 5"), SEM_6("Sem 6"), SEM_7("Sem 7"),
+	SEM_8("Sem 8");
+
+	private String alias;
+
+	private static final Map<String, Semister> by_alias = new HashMap<>();
+
+	static {
+		for (Semister sem : values()) {
+			by_alias.put(sem.getAlias(), sem);
+		}
+	}
+
+	public static Semister getByAlias(@NonNull String s) {
+		try {
+			return by_alias.get(s);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
