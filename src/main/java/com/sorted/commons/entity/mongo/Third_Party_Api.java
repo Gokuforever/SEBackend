2ae7ce1d1de0f8a4638291b6f625c09702986eb7
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2024-11-10 18:33:51 +0530

    Third_Party_Api:: new field added

--- src/main/java/com/sorted/commons/entity/mongo/Third_Party_Api.java
+++ src/main/java/com/sorted/commons/entity/mongo/Third_Party_Api.java
@@ -1,6 +1,7 @@
 package com.sorted.commons.entity.mongo;
 
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.http.HttpStatus;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,8 +15,10 @@ public class Third_Party_Api extends BaseMongoEntity<String> {/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private String request_type;
 	private String raw_request;
 	private String raw_response;
+	private HttpStatus status;
 	
 	
 
