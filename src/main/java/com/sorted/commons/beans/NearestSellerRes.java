bcf9a824e76d58edff28899c1dc94b58444748b3
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2024-12-18 23:24:48 +0530

    NearestSellerRes:: new bean added

---
+++ src/main/java/com/sorted/commons/beans/NearestSellerRes.java
@@ -0,0 +1,21 @@
+package com.sorted.commons.beans;
+
+import java.io.Serializable;
+
+import com.sorted.commons.porter.res.beans.GetQuoteResponse;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class NearestSellerRes implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String seller_id;
+	private GetQuoteResponse response;
+
+}
