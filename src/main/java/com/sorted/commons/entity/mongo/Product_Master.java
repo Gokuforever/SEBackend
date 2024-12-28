f63f4e5ef7e5728289a959d90bce617d04540c6e
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2024-12-28 16:25:13 +0530

    Product_Master:: document_id field added

--- src/main/java/com/sorted/commons/entity/mongo/Product_Master.java
+++ src/main/java/com/sorted/commons/entity/mongo/Product_Master.java
@@ -22,5 +22,6 @@ public class Product_Master extends BaseMongoEntity<String> {
 	private String catagory_id;
 	private String name;
 	private String img_src;
+	private String document_id;
 	private Map<String, List<String>> sub_categories;
 }
