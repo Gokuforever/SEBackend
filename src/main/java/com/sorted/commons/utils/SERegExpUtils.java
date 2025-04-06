8ca3730fe9cac43ac7bf9b55b7f36fed15279d5d
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-04-06 19:20:28 +0530

    Standard Text regex modified

--- src/main/java/com/sorted/commons/utils/SERegExpUtils.java
+++ src/main/java/com/sorted/commons/utils/SERegExpUtils.java
@@ -61,7 +61,8 @@ public class SERegExpUtils {
 		if (val.length() > 255) {
 			return false;
 		}
-		Pattern pattern = Pattern.compile("^(?!.*--)(?!.*&&)[a-zA-Z0-9,.-_()'&\\s]+$");
+//		Pattern pattern = Pattern.compile("^(?!.*--)(?!.*&&)[a-zA-Z0-9,.-_()'&\\s]+$");
+		Pattern pattern = Pattern.compile("^[A-Za-z0-9\\s\\-&.,':()]+$");
 		return pattern.matcher(val).matches();
 	}
 
