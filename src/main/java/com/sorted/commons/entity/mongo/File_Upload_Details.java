5f454635740525b061a2a59b3291c6481abd2026
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-06-04 01:10:27 +0530

    S3 image upload completed

--- src/main/java/com/sorted/commons/entity/mongo/File_Upload_Details.java
+++ src/main/java/com/sorted/commons/entity/mongo/File_Upload_Details.java
@@ -8,21 +8,22 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.FieldNameConstants;
 
+import java.io.Serial;
+
 @Data
 @EqualsAndHashCode(callSuper = false)
 @FieldNameConstants
 @Document(collection = "file_upload_details")
 public class File_Upload_Details extends BaseMongoEntity<String> {
 
-	/**
-	 * 
-	 */
+	@Serial
 	private static final long serialVersionUID = 1L;
 	private String file_id;
 	private UserType user_type;
 	private String entity_id;
 	private Integer document_type_id;
 	private String document_id;
+	private String file_url;
 	private String file_extension;
 	private String size;
 
---
+++ src/main/java/com/sorted/commons/utils/AwsS3Service.java
@@ -0,0 +1,135 @@
+package com.sorted.commons.utils;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.sorted.commons.beans.UsersBean;
+import com.sorted.commons.entity.mongo.File_Upload_Details;
+import com.sorted.commons.entity.service.File_Upload_Details_Service;
+import com.sorted.commons.enums.DocumentType;
+import com.sorted.commons.enums.ResponseCode;
+import com.sorted.commons.enums.UserType;
+import com.sorted.commons.exceptions.CustomIllegalArgumentsException;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class AwsS3Service {
+
+    private final File_Upload_Details_Service fileUploadDetailsService;
+
+    private final String accessKey;
+    private final String secretKey;
+    private final String region;
+    private final String bucketName;
+    private final String cloudFrontDomain;
+
+    private AmazonS3 s3Client;
+
+    private static final String BASE_FOLDER = "product-images/engineering-books/";
+
+    public AwsS3Service(
+            File_Upload_Details_Service fileUploadDetailsService,
+            @Value("${aws.access.key}") String accessKey,
+            @Value("${aws.secret.key}") String secretKey,
+            @Value("${aws.region}") String region,
+            @Value("${aws.s3.bucket.name}") String bucketName,
+            @Value("${aws.cloudfront.distribution.domain}") String cloudFrontDomain
+    ) {
+        this.fileUploadDetailsService = fileUploadDetailsService;
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.region = region;
+        this.bucketName = bucketName;
+        this.cloudFrontDomain = cloudFrontDomain;
+    }
+
+    @PostConstruct
+    private void initializeAmazon() {
+        log.info("Initializing AWS S3 Client with region: {}", region);
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        this.s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+    public File_Upload_Details uploadPhoto(MultipartFile multipartFile, UsersBean usersBean, DocumentType documentType) throws IOException {
+        log.info("Starting upload for user ID: {}, documentType: {}", usersBean.getId(), documentType);
+
+        // 1. Validate image type
+        if (!CommonUtils.isImage(multipartFile)) {
+            log.warn("Invalid file type uploaded by user ID: {}", usersBean.getId());
+            throw new CustomIllegalArgumentsException(ResponseCode.INVALID_FILE_TYPE);
+        }
+
+        // 2. Check access rights
+        UserType userType = usersBean.getRole().getUser_type();
+        if (!documentType.getAllowed_to().contains(userType)) {
+            log.error("Access denied: UserType {} is not allowed to upload document type {}", userType, documentType);
+            throw new CustomIllegalArgumentsException(ResponseCode.ACCESS_DENIED);
+        }
+
+        // 3. Prepare file metadata
+        String fileName = generateFileName(multipartFile);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        log.info("Generated filename: {}", fileName);
+
+        // 4. Upload to S3
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, multipartFile.getInputStream(), metadata));
+        log.info("Successfully uploaded file to S3: bucket={}, key={}", bucketName, fileName);
+
+        // 5. Save file metadata in DB
+        File_Upload_Details details = new File_Upload_Details();
+        populateUploadDetails(details, usersBean, userType, documentType);
+        details.setFile_url(getFileUrl(fileName));
+        details.setFile_extension(getExtension(Objects.requireNonNull(multipartFile.getOriginalFilename())));
+        double sizeKB = multipartFile.getSize() / 1024.0;
+        details.setSize(String.format("%.2fkb", sizeKB));
+
+        File_Upload_Details savedDetails = fileUploadDetailsService.create(details, usersBean.getId());
+        log.info("Metadata stored in DB with ID: {}", savedDetails.getId());
+
+        return savedDetails;
+    }
+
+    private String getFileUrl(String fileName) {
+        return "https://" + cloudFrontDomain + "/" + fileName;
+    }
+
+    private String generateFileName(MultipartFile file) {
+        String original = Objects.requireNonNull(file.getOriginalFilename()).replace(" ", "_");
+        return BASE_FOLDER + UUID.randomUUID() + "-" + original + CommonUtils.generateFixedLengthRandomNumber(3);
+    }
+
+    private String getExtension(String fileName) {
+        int dotIndex = fileName.lastIndexOf(".");
+        return (dotIndex >= 0) ? fileName.substring(dotIndex + 1) : "";
+    }
+
+    private void populateUploadDetails(File_Upload_Details details, UsersBean user, UserType userType, DocumentType docType) {
+        if (userType == UserType.SELLER) {
+            details.setEntity_id(user.getRole().getSeller_id());
+        } else {
+            details.setEntity_id(user.getId());
+        }
+        details.setUser_type(userType);
+        details.setDocument_type_id(docType.getId());
+    }
+}
