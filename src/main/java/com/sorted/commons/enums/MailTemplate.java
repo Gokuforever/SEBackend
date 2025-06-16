f7166694ed08a635ff43f602e1258e082335aa71
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-06-17 00:26:28 +0530

    New mailtemplate added

--- src/main/java/com/sorted/commons/enums/MailTemplate.java
+++ src/main/java/com/sorted/commons/enums/MailTemplate.java
@@ -10,6 +10,7 @@ public enum MailTemplate {
 	DIRECT_ORDER_CONFIRMATION("direct_order_confirmation.html", "Order Confirmation!."),
 	ORDER_ARRIVED("order_arrived.html", "Your order has arrived."),
 	ORDER_DISPATCHED("order_dispatched.html", "Your order is on the way."),
+	NEW_ORDER_ARRIVED("new_order_arrived.html", "Order from Studeaze!."),
 	SELLER_WELCOME_MAIL("welcome_mail.html", "Welcome to Studeaze.");
 
 	private final String file_name;
