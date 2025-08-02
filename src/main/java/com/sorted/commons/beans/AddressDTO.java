4d42a35347f5f3a4cff631434f95afe58e9d5e20
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-08-03 00:21:07 +0530

    Minor changes

--- src/main/java/com/sorted/commons/beans/AddressDTO.java
+++ src/main/java/com/sorted/commons/beans/AddressDTO.java
@@ -4,28 +4,37 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
-public class AddressDTO implements Serializable{
+public class AddressDTO implements Serializable {
 
-	/**
-	 * 
-	 */
-	@Serial
-	private static final long serialVersionUID = 1L;
-	private String id;
-	private String code;
-	private String street_1;
-	private String street_2;
-	private String landmark;
-	private String city;
-	private String state;
-	private String pincode;
-	private String address_type;
-	private String address_type_desc;
-	private Boolean is_default;
-	private BigDecimal lat;
-	private BigDecimal lng;
-	private String phone_no;
+    /**
+     *
+     */
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private String id;
+    private String code;
+    private String street_1;
+    private String street_2;
+    private String landmark;
+    private String city;
+    private String state;
+    private String pincode;
+    private String address_type;
+    private String address_type_desc;
+    private Boolean is_default;
+    private BigDecimal lat;
+    private BigDecimal lng;
+    private String phone_no;
+
+    @JsonIgnore
+    public String getFullAddress() {
+        if (street_2 == null) {
+            return street_1 + ", " + landmark + ", " + city + ", " + state + ", " + pincode;
+        }
+        return street_1 + ", " + street_2 + ", " + landmark + ", " + city + ", " + state + ", " + pincode;
+    }
 }
