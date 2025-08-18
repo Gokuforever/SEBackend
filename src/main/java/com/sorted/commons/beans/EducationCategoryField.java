2465e2a37a7e2f80be8e3cea3a726d79db813b30
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-08-19 00:37:46 +0530

    Removed JsonIgnore

--- src/main/java/com/sorted/commons/beans/EducationCategoryField.java
+++ src/main/java/com/sorted/commons/beans/EducationCategoryField.java
@@ -1,7 +1,6 @@
 package com.sorted.commons.beans;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.List;
@@ -13,7 +12,6 @@ public class EducationCategoryField {
     private String type;
     private List<String> options;
     private boolean mandatory;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String description;
     @JsonIgnore
     private boolean filterable;
