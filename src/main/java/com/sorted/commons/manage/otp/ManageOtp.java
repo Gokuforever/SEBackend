df25f485c82a32e3c0ee8b66284278d3182e9e07
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-08-26 17:55:49 +0530

    OTP enabled

--- src/main/java/com/sorted/commons/manage/otp/ManageOtp.java
+++ src/main/java/com/sorted/commons/manage/otp/ManageOtp.java
@@ -63,7 +63,7 @@ public class ManageOtp {
 
         Otp otp = new Otp();
         String random_otp;
-        if ("prod-final".equalsIgnoreCase(profile)) {
+        if ("prod".equalsIgnoreCase(profile)) {
             random_otp = CommonUtils.generateFixedLengthRandomNumber(otp_length);
         } else {
             random_otp = "111111";
@@ -116,7 +116,7 @@ public class ManageOtp {
         smsPool.setMobile_no(mobileNumber);
         smsPool.setContent(content);
         smsPool = smsPool_Service.create(smsPool, Defaults.SMS_SERVICE);
-        if ("prod-final".equalsIgnoreCase(profile)) {
+        if ("prod".equalsIgnoreCase(profile)) {
             try {
                 String body = "{\r\n    \"route\": \"otp\",\r\n    \"variables_values\": \"" + content
                         + "\",\r\n    \"numbers\": \"" + mobileNumber + "\"\r\n}";
