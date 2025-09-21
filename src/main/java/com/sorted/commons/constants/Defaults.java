bfb5482bd2d66443d46cd89e7bc77b8a6860e3cb
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-09-21 18:03:18 +0530

    Minor change

--- src/main/java/com/sorted/commons/constants/Defaults.java
+++ src/main/java/com/sorted/commons/constants/Defaults.java
@@ -21,4 +21,5 @@ public class Defaults {
 	public static final String REACT_APP = "React App";
 	public static final String AUTH = "Auth";
 	public static final String PHONEPE_REFUND_CRON = "PhonePe Refund Cron";
+	public static final String RETOOL = "Retool";
 }
