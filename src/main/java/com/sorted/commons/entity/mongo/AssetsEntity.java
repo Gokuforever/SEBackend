abff624e27d0ad7e2cb7ba6e1aa0a3a5547bd513
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-09-28 13:21:02 +0530

    Minor change

---
+++ src/main/java/com/sorted/commons/entity/mongo/AssetsEntity.java
@@ -0,0 +1,23 @@
+package com.sorted.commons.entity.mongo;
+
+import com.sorted.commons.enums.AssetType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldNameConstants;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@FieldNameConstants
+@Document(collection = "assets")
+public class AssetsEntity extends BaseMongoEntity<String> {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private AssetType type;
+    private String url;
+    private int order;
+}
